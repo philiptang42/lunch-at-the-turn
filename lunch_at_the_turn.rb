ffc9f94d27d14ef8e60c8a2dcd2f78bcc4926ffe
philiptang42 <philiptang42@gmail.com>
2015-11-26 17:13:28 -0500

    solution

--- lunch_at_the_turn.rb
+++ lunch_at_the_turn.rb
@@ -1,8 +1,34 @@
 menu = {
-  Hamburger: "$4.00"
-  Hot_Dog: "$3.00"
-  Fries: "$2.00"
-  Chips: "$1.00"
-  Water: "$1.25"
-  Soda: "$1.50"
+  "hamburger" => 400,
+  "hot dog" => 300,
+  "fries" => 200,
+  "chips" => 100,
+  "water" => 125,
+  "soda" => 150
 }
+
+items_nonexistant = nil
+items = []
+
+while items_nonexistant.nil? || !items_nonexistant.empty?
+  puts "So what do you want?"
+  order = gets.chomp
+  order.downcase!
+
+  items = order.split(", ")
+
+  items_nonexistant = items - menu.keys
+
+  if !items_nonexistant.empty?
+    items_nonexistant.each do |thing|
+      puts "#{thing} isn't served here."
+    end
+  end
+end
+
+total = 0
+items.each do |item|
+  total += menu[item]
+end
+
+puts "The total is $#{'%.2f' % (total / 100.to_f)}."
